8b9595e1eab71af0d6ae1c463a25d76227109017
Raylan LIN <linrion101@gmail.com>
2026-04-25 08:59:03 +0800

    fix: renderer loading path in production (off by one directory level)

--- src/main/index.ts
+++ src/main/index.ts
@@ -53,7 +53,7 @@ function createMainWindow(): void {
     mainWindow.loadURL('http://localhost:5173');
     mainWindow.webContents.openDevTools({ mode: 'detach' });
   } else {
-    mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
+    mainWindow.loadFile(path.join(app.getAppPath(), 'dist/renderer/index.html'));
   }
 
   mainWindow.on('closed', () => {
